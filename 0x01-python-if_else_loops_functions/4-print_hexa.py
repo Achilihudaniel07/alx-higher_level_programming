9726167e2869c87cbc82ab77da09a814f29831de
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-11-29 22:07:29 +0100

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,17 +1,3 @@
 #!/usr/bin/python3
-
-import random
-
-number = random.randint(-10, 10)
-
-if number > 0:
-
-    print("{:d} is positive".format(number))
-
-elif number == 0:
-
-    print("{:d} is zero".format(number))
-
-else:
-
-    print("{:d} is negative".format(number))
+for num in range(99):
+    print("{:d} = {:s}".format(num, hex(num)))
