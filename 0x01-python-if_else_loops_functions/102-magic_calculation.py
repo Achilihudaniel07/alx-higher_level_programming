590125a39ff6cfb1bd4cf66c3aed7e8bded4aa9b
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-11-29 22:25:26 +0100

    Update 102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-
-import random
-
-number = random.randint(-10, 10)
-
-if number > 0:
-
-    print("{:d} is positive".format(number))
-
-elif number == 0:
-
-    print("{:d} is zero".format(number))
-
-else:
-
-    print("{:d} is negative".format(number))
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    else:
+        if c > b:
+            return a + b
+    return (a * b) - c
