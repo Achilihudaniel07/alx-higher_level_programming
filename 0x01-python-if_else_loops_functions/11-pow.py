a17d811517b3aa8ab137c69ab06b76c133b4cfda
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-11-29 22:04:46 +0100

    Update 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,17 +1,2 @@
 #!/usr/bin/python3
-
-import random
-
-number = random.randint(-10, 10)
-
-if number > 0:
-
-    print("{:d} is positive".format(number))
-
-elif number == 0:
-
-    print("{:d} is zero".format(number))
-
-else:
-
-    print("{:d} is negative".format(number))
+def pow(a, b): return a ** b
