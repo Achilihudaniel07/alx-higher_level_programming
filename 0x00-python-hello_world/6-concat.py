e7d18a4d1f245c111f5b2debe5aaeb4314ebc71c
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-11-29 05:53:44 +0100

    Update 6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,1 +1,5 @@
-print("\"Programming is like building a multilingual puzzle")
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print("Welcome to {}!".format(str1))
