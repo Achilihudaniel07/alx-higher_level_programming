2bbfa975d600ba88004b8f019ce2429220b37ebd
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-01 12:07:53 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
-    from add_0 import add
-
-    a = 1
-
-    b = 2
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
