1004741d2c3baf83c9d2517c005ac82c224f2627
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-01 12:08:15 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
-    from add_0 import add
-
-    a = 1
-
-    b = 2
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
