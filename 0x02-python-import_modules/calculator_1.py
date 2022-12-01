c80ee17089f2a973fa8cef06c9f5d2529f165e5a
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-01 12:09:24 +0100

    Update calculator_1.py

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,11 +1,43 @@
 #!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
 
-if __name__ == "__main__":
 
-    from add_0 import add
+def sub(a, b):
+    """My subtraction function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
 
-    a = 1
 
-    b = 2
+def mul(a, b):
+    """My multiplication function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
+
+def div(a, b):
+    """My division function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
