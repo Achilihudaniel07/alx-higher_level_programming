941c83862539b73a51d662f5227e0c973ef8f723
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-02 08:09:55 +0100

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,13 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
 
-def print_list_integer(my_list=[]):
+    length = len(my_list)
 
-    for i in range(len(my_list)):
+    if idx > length - 1:
+        return (my_list)
 
-        print("{:d}".format(my_list[i]))
+    my_list[idx] = element
+
+    return (my_list)
