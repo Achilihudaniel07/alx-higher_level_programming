3dff5fa28ab4bbb77e94bddb52a5785e7ea74db5
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-02 08:14:19 +0100

    Update lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,7 +1,27 @@
- #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
+#ifndef LISTS_H
+#define LISTS_H
 
-    for i in range(len(my_list)):
+#include <stdlib.h>
 
-        print("{:d}".format(my_list[i]))
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+int is_palindrome(listint_t **head);
+
+#endif /* LISTS_H */
