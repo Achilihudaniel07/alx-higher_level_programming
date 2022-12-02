f7b7d995df30855929c1a522e232c65b22eb3e83
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-02 08:07:34 +0100

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,11 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return (None)
 
-def print_list_integer(my_list=[]):
+    length = len(my_list)
 
-    for i in range(len(my_list)):
+    if idx > length - 1:
+        return (None)
 
-        print("{:d}".format(my_list[i]))
+    return(my_list[idx])
