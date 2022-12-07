d5e412db891d87aeb3085e883a944e9184a4442b
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-07 09:52:44 +0100

    Update 11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,15 +1,3 @@
-/usr/bin/python3
-
-def square_matrix_simple(matrix=[]):
-
-    new_matrix = matrix.copy()
-
-
-
-    for i in range(len(matrix)):
-
-        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-
-
-
-    return (new_matrix)
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    return(list(map(lambda elem: (elem * number), my_list)))
