22f05dd0a4f7101a20f2de7deeccacf3980a60d4
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-19 18:50:03 +0100

    Update 6-raise_exception_msg.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
 
-safe_print_list = __import__(0-safe_print_list).safe_print_list
 
-
-
-my_list = [1, 2, 3, 4, 5]
-
-
-
-nb_print = safe_print_list(my_list, 2)
-
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, len(my_list))
-
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-
-print("nb_print: {:d}".format(nb_print))
+def raise_exception_msg(message=""):
+    """
+    A function that raises a name exception
+    with a message
+    """
+    raise NameError(message)
