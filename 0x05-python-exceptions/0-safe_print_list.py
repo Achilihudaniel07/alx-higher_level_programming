ebb0ef9d5486de53e06f15a3b33e95049780f1bd
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-19 18:40:47 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 
-safe_print_list = __import__(0-safe_print_list).safe_print_list
 
-
-
-my_list = [1, 2, 3, 4, 5]
-
-
-
-nb_print = safe_print_list(my_list, 2)
-
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, len(my_list))
-
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-
-print("nb_print: {:d}".format(nb_print))
+def safe_print_list(my_list=[], x=0):
+    """
+    A function that prints x elements of a list.
+    """
+    list_count = 0
+    for element in range(x):
+        try:
+            print("{:d}".format(my_list[element]), end="")
+            list_count += 1
+        except IndexError:
+            break
+    print("")
+    return list_count
