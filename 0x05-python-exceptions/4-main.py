d3e8da221fedfa03f6c3a6d271458e16609905c7
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-19 18:48:21 +0100

    Update 4-main.py

--- 0x05-python-exceptions/4-main.py
+++ 0x05-python-exceptions/4-main.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
+list_division = __import__('4-list_division').list_division
 
-safe_print_list = __import__(0-safe_print_list).safe_print_list
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
 
+print("--")
 
-
-my_list = [1, 2, 3, 4, 5]
-
-
-
-nb_print = safe_print_list(my_list, 2)
-
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, len(my_list))
-
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-
-print("nb_print: {:d}".format(nb_print))
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
