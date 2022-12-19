84a6e32e0e4524bed85325e02d5fd305fc4ba31b
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-19 18:48:46 +0100

    Update 5-main.py

--- 0x05-python-exceptions/5-main.py
+++ 0x05-python-exceptions/5-main.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
+raise_exception = __import__('5-raise_exception').raise_exception
 
-safe_print_list = __import__(0-safe_print_list).safe_print_list
-
-
-
-my_list = [1, 2, 3, 4, 5]
-
-
-
-nb_print = safe_print_list(my_list, 2)
-
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, len(my_list))
-
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-
-print("nb_print: {:d}".format(nb_print))
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
