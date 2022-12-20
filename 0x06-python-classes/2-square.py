99eaf3f2ff87997e8634c07e503ec7a7d7e6fdbf
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-20 07:19:34 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-
-"""Empty Class Square"""
-
-
-
+"""Create a Class Square with size"""
 
 
 class Square:
+    """Class - Square"""
 
-    """Empty Class Square"""
-
-    pass
+    def __init__(self, size=0):
+        """Constructor of a Square with the size"""
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
