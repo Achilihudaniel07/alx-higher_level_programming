c7ff6cebcec9b59562f004da5c3040e4e6b66bdb
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-01-10 06:29:58 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,8 +5,10 @@
 def read_file(filename=""):
     """a function that reads a text file (UTF8)
     and prints it to stdout
+
     Args:
         filename(any): name of the file
+
     """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
