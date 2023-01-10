e56140c83734b8fd0ee22a0f5642f86f2058c156
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-01-10 06:44:49 +0100

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,12 @@
-kesh
+#!/usr/bin/python3
+"""Create object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """A function that creates an Object
+    from a “JSON file”
+
+    """
+    with open(filename) as f:
+        return json.load(f)
