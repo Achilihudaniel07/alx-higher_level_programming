08f337e0a222794d9c9bbf512347e476fa9c49d7
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-01-10 06:46:08 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,11 @@
-kesh
+#!/usr/bin/python3
+"""Class to JSON
+"""
+
+
+def class_to_json(obj):
+    """Return the dictionary representation
+    of a simple data structure.
+
+    """
+    return obj.__dict__
