d992b1be8dc3ce20df08f3f81d53351184093e00
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-01-10 06:38:47 +0100

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,13 @@
-kesh
+#!/usr/bin/python3
+"""To JSON string"""
+import json
+
+
+def to_json_string(my_obj):
+    """A JSON to string function
+
+    Return:
+        the JSON representation of an object (string):
+
+    """
+    return json.dumps(my_obj)
