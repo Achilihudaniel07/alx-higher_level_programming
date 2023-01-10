a488d820b2f3675ed035c864f62a661b5e38718d
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-01-10 06:39:31 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,11 @@
-kesh
+#!/usr/bin/python3
+"""From JSON string to Object"""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a
+    JSON string.
+
+    """
+    return json.loads(my_str)
