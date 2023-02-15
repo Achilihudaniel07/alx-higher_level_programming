d567c060ca43e0e38fabfd0c93bb5b7aeeb3b817
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-02-15 18:17:19 +0100

    Update 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,1 +1,4 @@
-sql_introduction
+-- Write a script that creates a table called first_table in the current database in your MySQL server.
+
+CREATE TABLE first_table (
+  id INT, name VARCHAR(256));
