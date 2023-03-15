c0bbcefd6d56ade0d2f6c32e5c969b6f215e75e1
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-03-15 15:27:09 +0100

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,12 @@
-0x13-javascript_objects_scopes_closures
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
