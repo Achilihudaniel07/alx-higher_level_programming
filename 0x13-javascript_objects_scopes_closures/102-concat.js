dede269ad897bcf526cd5fdf1b892a8af3acc0c3
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-03-15 15:26:49 +0100

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,24 @@
-0x13-javascript_objects_scopes_closures
+#!/usr/bin/node
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+const fs = require('fs');
+
+let text = '';
+if (fileA !== undefined && fileB !== undefined &&
+    fileC !== undefined) {
+  if ((fs.existsSync(fileA) && fs.statSync(fileA).isFile) &&
+    (fs.existsSync(fileB) && fs.statSync(fileB).isFile)) {
+    text += fs.readFileSync(fileA, (error) => {
+      if (error) throw error;
+    });
+
+    text += fs.readFileSync(fileB, (error) => {
+      if (error) throw error;
+    });
+
+    fs.writeFile(fileC, text, (error) => {
+      if (error) throw error;
+    });
+  }
+}
