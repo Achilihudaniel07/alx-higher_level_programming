bbbcbc3ee551c63457867f5ced35b819e20b6fbe
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-03-20 06:17:52 +0100

    Update 6-peak.py

--- 0x0F-python-object_relational_mapping/6-peak.py
+++ 0x0F-python-object_relational_mapping/6-peak.py
@@ -1,1 +1,9 @@
-0x0F-python-object_relational_mapping
+#!/usr/bin/python3
+"""Algorithms for list of integers."""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
