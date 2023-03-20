a359cbd1f19b3486d3ee71c81339bfc3f2feb63c
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-03-20 06:17:27 +0100

    Update 5-post_params.sh

--- 0x0F-python-object_relational_mapping/5-post_params.sh
+++ 0x0F-python-object_relational_mapping/5-post_params.sh
@@ -1,1 +1,3 @@
-0x0F-python-object_relational_mapping
+#!/bin/bash
+# Sends a POST request to the passed URL, and displays the body of the response
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" "${1}"
