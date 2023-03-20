920f74e3046fdc4334c718cec9a8f21f8a4aa693
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-03-20 06:12:06 +0100

    Update 101-post_json.sh

--- 0x0F-python-object_relational_mapping/101-post_json.sh
+++ 0x0F-python-object_relational_mapping/101-post_json.sh
@@ -1,1 +1,3 @@
-0x0F-python-object_relational_mapping
+#!/bin/bash
+# Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
