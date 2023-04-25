23c95b57c5cc6a7ce02a99e2e7d7341479122979
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-04-25 14:39:37 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,6 @@
-0x14-javascript-web_scraping
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
