5f8ae957831e002ec7f5cc9b9a81ece74ecbbb26
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-05-04 00:31:14 +0100

    Update 102-script.js

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,8 @@
-0x15-javascript-web_jquery
+$('document').ready(function () {
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  $('INPUT#btn_translate').click(function () {
+    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+      $('DIV#hello').html(data.hello);
+    });
+  });
+});
